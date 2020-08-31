a554367a3c9ddc2c3b144b34ea52bb819deb6003
siyangc <csy602994381@gmail.com>
2020-08-31 11:42:26 +1000

    fix the cursor pointer not work bug

--- src/components/topNavBar/topNavBarRight.js
+++ src/components/topNavBar/topNavBarRight.js
@@ -1,68 +1,36 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 //import githubIcon from '../../public/icons/GitHub-Mark-32px.png'
-import {Mobile, Default} from '../../configs/mediaQueryBreakPoint'
+//import {Mobile, Default} from '../../configs/mediaQueryBreakPoint'
+import './topNavBarRight.css';
 
 export default class topNavBarRight extends Component {
     
     render() {        
         return (
-            <div style={topNavBarRightStyle}>
-                <Default>                    
-                    <ul style={listStyle}>
-                        <li style={itemStyle}>
-                            <img style={iconStyle}
-                                    src="/icons/gitHub.png" 
-                                    alt="github"
-                                    onClick = {e => window.open("https://github.com/siyangc",'_blank')}                                  
-                            />
-                        </li>
-                        <li style={itemStyle}>
-                            <img style={iconStyle}
-                                    src="/icons/facebook.png" 
-                                    alt="facebook"
-                                    onClick = {e => window.open("https://www.facebook.com/smrcsy",'_blank')}                                    
-                                    
-                            />
-                        </li>
-                        <li style={itemStyle}>
-                            <img style={iconStyle}
-                                    src="/icons/linkedin.png" 
-                                    alt="linkedin"
-                                    onClick = {e => window.open("https://www.linkedin.com/in/siyangc",'_blank')}                                        
-                            />
-                        </li>
-                    </ul>
-                    
-                </Default>
-                <Mobile>
-                    haha
-                </Mobile>
-            </div>
-            
+            <div className="topNavBarRight">                
+                <ul>
+                    <li>
+                        <img    src="/icons/gitHub.png" 
+                                alt="github"
+                                onClick = {e => window.open("https://github.com/siyangc",'_blank')}                                  
+                        />
+                    </li>
+                    <li>
+                        <img    src="/icons/facebook.png" 
+                                alt="facebook"
+                                onClick = {e => window.open("https://www.facebook.com/smrcsy",'_blank')}                                    
+                                
+                        />
+                    </li>
+                    <li>
+                        <img    src="/icons/linkedin.png" 
+                                alt="linkedin"
+                                onClick = {e => window.open("https://www.linkedin.com/in/siyangc",'_blank')}                                        
+                        />
+                    </li>
+                </ul>                   
+            </div>            
         )
     }
 }
 
-
-const topNavBarRightStyle = {
-    width: "100%",
-    paddingRight: "5px"
-}
-const listStyle = {    
-    listStyle: "none",
-    display: "flex",
-    justifyContent: "flex-end",
-    height: "30px"
-}
-
-const itemStyle = {
-    width: "30px",
-    marginLeft: "5px",
-    marginRight: "10px",
-    padding: "5px",
-}
-
-const iconStyle = {
-    width: "20px",
-    cursor: "pointer"
-}
